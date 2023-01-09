b8916999c2948ac16df9aee8c3572a505eabfc2f
Paul Falgout <paul.falgout@roundingwell.com>
2023-01-10 03:18:43 +0900

    WIP

--- src/js/entities-service/entities/files.js
+++ src/js/entities-service/entities/files.js
@@ -4,9 +4,75 @@ import BaseModel from 'js/base/model';
 
 const TYPE = 'files';
 
+function selectFile(contentType) {
+  return new Promise(resolve => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = contentType;
+
+    input.onchange = () => {
+      const files = Array.from(input.files);
+      resolve(files[0]);
+    };
+
+    input.click();
+  });
+}
+
 const _Model = BaseModel.extend({
+  defaults: {
+    path: '',
+  },
   type: TYPE,
-  urlRoot: '/api/files',
+  urlRoot() {
+    if (this.isNew()) {
+      const actionId = this.get('_action');
+
+      return `/api/actions/${ actionId }/relationships/files?urls=upload`;
+    }
+    return '/api/files';
+  },
+  fetchFile() {
+    return this.fetch({ data: { urls: ['view', 'download'] } });
+  },
+  createUpload(fileName) {
+    const path = `patient/${ this.get('_patient') }/${ fileName }`;
+    return this.save({ path });
+  },
+  upload() {
+    selectFile('.pdf').then(file => {
+      this.createUpload(file.name).then(() => {
+        const uploadUrl = this.get('_upload');
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+              // alert('UPLOAD FAILED');
+              this.destroy();
+
+              return;
+            }
+            this.set({ _progress: 100 });
+            this.fetchFile();
+          }
+        };
+
+        xhr.upload.onprogress = e => {
+          if (e.lengthComputable) {
+            this.set({ _progress: (e.loaded / file.size) * 100 });
+          }
+        };
+
+        xhr.open('PUT', uploadUrl);
+        xhr.send(file);
+      });
+    });
+  },
+  getFilename() {
+    return this.get('path').split('/').pop();
+  },
 });
 
 const Model = Store(_Model, TYPE);
--- src/js/views/patients/sidebar/action/action-sidebar-attachments-views.js
+++ src/js/views/patients/sidebar/action/action-sidebar-attachments-views.js
@@ -9,17 +9,31 @@ import './action-sidebar.scss';
 
 const AttachmentView = View.extend({
   className: 'u-margin--t-16',
-  template: hbs`
+  modelEvents: {
+    'change: _progress': 'render',
+    'change: _download': 'render',
+  },
+  downloadTemplate: hbs`
     <a class="action-sidebar__attachment-filename" target="_blank" href="{{_view}}">{{filename}}</a>
     <div>
       <a class="action-sidebar__attachment-download" href="{{_download}}" download>
-        {{far "download"}} <span>{{ @intl.patients.sidebar.action.attachmentsViews.attachmentView.downloadText }}</span>
+        {{far "download"}} <span>{{ @intl.patients.sidebar.action.actionSidebarAttachmentsViews.attachmentView.downloadText }}</span>
       </a>
     </div>
   `,
+  uploadTemplate: hbs`
+    <div class="action-sidebar__attachment-filename">{{filename}}</div>
+    <progress value="_progress" max="100" class="action-sidebar__attachment-progress"></progress>
+  `,
+  getTemplate() {
+    if (this.get('_download')) {
+      return this.downloadTemplate;
+    }
+    return this.uploadTemplate;
+  },
   templateContext() {
     return {
-      filename: this.model.get('path').split('/').pop(),
+      filename: this.model.getFilename(),
     };
   },
 });
@@ -29,15 +43,22 @@ const AttachmentsView = CollectionView.extend({
   template: hbs`
     <div class="sidebar__attachments">
       <h3 class="sidebar__heading">
-        {{far "paperclip"}}<span class="u-margin--l-8">{{ @intl.patients.sidebar.action.attachmentsViews.attachmentsHeadingText }}</span>
+        {{far "paperclip"}}<span class="u-margin--l-8">{{ @intl.patients.sidebar.action.actionSidebarAttachmentsViews.attachmentsViews.attachmentsHeadingText }}</span>
       </h3>
       <div data-attachments-files-region></div>
+      <button class="button-primary u-margin--t-16 js-add">{{far "paperclip"}}<span>{{ @intl.patients.sidebar.action.actionSidebarAttachmentsViews.attachmentsViews.addAttachment }}</span></button>
     </div>
   `,
   childViewContainer: '[data-attachments-files-region]',
   childView: AttachmentView,
   viewComparator(viewA, viewB) {
     return alphaSort('desc', viewA.model.get('created_at'), viewB.model.get('created_at'));
+  },
+  viewFilter({ model }) {
+    return !model.isNew() || !model.get('_download');
+  },
+  triggers: {
+    'click .js-add': 'click:add',
   },
 });
 
